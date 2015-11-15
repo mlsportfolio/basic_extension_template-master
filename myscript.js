57f909ada2ac79d091cf3ab441f99a917fb05e19
thehacker1 <mshroff@lsoc.org>
2015-11-15 16:09:57 -0600

    FBI Hack

---
+++ myscript.js
@@ -0,0 +1,49 @@
+
+
+if(window.location.href.indexOf('espn') > -1) {
+	document.querySelector('body').innerHTML = ""
+document.querySelector("body").style.backgroundColor = "black";
+
+	var image = document.createElement('img')
+	image.src = "https://upload.wikimedia.org/wikipedia/commons/0/0b/Silk_Road_Seized.jpg"
+	document.querySelector("body").appendChild(image)
+
+
+
+
+
+
+
+
+// 	alert('ERROR: 401: Website is not resonding...');
+// 	alert('Virus A370159 has hacked your computer. It has infected over 1 million computers. Once infected, your computer will annoy you.');
+// 	alert('To get it off your computer, leave this webite and never come back. ');
+// 	alert('Please try again later...');
+// 	alert('Please try again later...');
+// alert('Please try again later...');
+// alert('Please try again later...');
+// alert('Please try again later...');
+// alert('Please try again later...');
+// alert('Please try again later...');
+// alert('Please try again later...');
+// alert('Please try again later...');
+// alert('I told you to try again later.');
+// alert('I told you to try again later.');
+// alert('Ok you will be able to go back to the page in 3...');
+// alert('2...');
+// alert('2...');
+// alert('2...');
+// alert('1...');
+// alert('2...');
+// alert('3...');
+// alert('1...');
+// alert('0...');
+// alert('Have a great day!');
+// alert('I really wish you have a great day!');
+// alert('Bye!!');
+// alert('Love you!');
+
+
+
+
+}
